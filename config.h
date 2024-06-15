49076ef9146657820069df01aa8e125c44f61525
xaizone <git@xaizone.eu>
2024-06-15 18:51:48 +0200

    apply colorbar patch

--- config.h
+++ config.h
@@ -1,5 +1,3 @@
-/* See LICENSE file for copyright and license details. */
-
 /* constants */
 #define TERMINAL "alacritty"
 #define BROWSER "firefox"
@@ -11,25 +9,35 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=12" };
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#7c6f64";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_gray4[]       = "#282828";
+static const char col_cyan[]        = "#fabd2f";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /* scheme              fg         bg         border   */
+
+    /* window */
+    [SchemeNorm]       = { col_gray3, col_gray1, col_gray2 },
+    [SchemeSel]        = { col_gray4, col_cyan,  col_cyan  },
+
+    /* statusbar */
+    [SchemeStatus]     = { col_gray3, col_gray1, "#000000" },
+
+    /* tagbar */
+    [SchemeTagsSel]    = { col_gray4, col_cyan,  "#000000" },
+    [SchemeTagsNorm]   = { col_gray3, col_gray1, "#000000" },
+
+    /* infobar */
+    [SchemeInfoSel]    = { col_gray3, col_gray1, "#000000" },
+    [SchemeInfoNorm]   = { col_gray3, col_gray1, "#000000" },
+
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "^steam$",  NULL,       NULL,       0,            1,           -1 },
 	{ "^Steam$",  NULL,       NULL,       0,            1,           -1 },
@@ -37,20 +45,19 @@ static const Rule rules[] = {
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "vesktop",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "steam",    NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "lutris",   NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "obs",      NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact        = 0.5;  /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;    /* number of clients in master area */
+static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },
+	{ "><>",      NULL },
 	{ "[M]",      monocle },
 };
 
@@ -154,4 +161,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
