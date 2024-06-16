20e3b9caefe5bd6a153d02462169e841f89a7ea7
xaizone <git@xaizone.eu>
2024-06-16 09:17:30 +0200

    colors

--- config.h
+++ config.h
@@ -4,22 +4,17 @@
 #define FM "nnn"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=12" };
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#7c6f64";
-static const char col_gray3[]       = "#ebdbb2";
-static const char col_gray4[]       = "#282828";
-static const char col_cyan[]        = "#fabd2f";
 static const char *colors[][3]      = {
     /* scheme              fg         bg         border   */
 
     /* window */
-    [SchemeNorm]       = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]        = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeNorm]       = { "#707070", "#1d1f21", "#282a2e" },
+    [SchemeSel]        = { "#c5c8c6", "#1d1f21", "#f0c674" },
 
 };
 
@@ -28,9 +23,6 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "^steam$",  NULL,       NULL,       0,            1,           -1 },
-	{ "^Steam$",  NULL,       NULL,       0,            1,           -1 },
-	{ "^steam$",  NULL,       "^Steam$",  0,            0,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "vesktop",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "steam",    NULL,       NULL,       1 << 3,       0,           -1 },
